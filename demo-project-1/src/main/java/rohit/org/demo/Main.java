09d2177ec87598d525224ed70aa8430d69c26efd
rohit <rohit.kushwah@tessell.com>
2023-07-24 12:47:06 +0530

    text changed further

--- demo-project-1/src/main/java/rohit/org/demo/Main.java
+++ demo-project-1/src/main/java/rohit/org/demo/Main.java
@@ -2,6 +2,6 @@ package rohit.org.demo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! this is my demo prpject");
+        System.out.println("Hello world! this is my demo prpject version 2nd");
     }
 }
