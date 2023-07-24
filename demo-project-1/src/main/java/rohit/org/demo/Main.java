d3be162f6716d5d7a5dea2c67fee21e8bbb4725d
rohit <rohit.kushwah@tessell.com>
2023-07-24 13:01:36 +0530

    another line added

--- demo-project-1/src/main/java/rohit/org/demo/Main.java
+++ demo-project-1/src/main/java/rohit/org/demo/Main.java
@@ -3,5 +3,7 @@ package rohit.org.demo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! this is my demo prpject version 2nd");
+        System.out.println("This is my change in second line");
     }
+
 }
